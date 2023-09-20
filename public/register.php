783802f59439bff0c1c30247c7c699c83b343a53
ronirbr <roni.ribeiro.n00@gmail.com>
2023-09-20 11:42:50 -0300

    make the registration form more organized

--- public/register.php
+++ public/register.php
@@ -1,12 +1,11 @@
-<!doctype html>
-<html lang="pt-BR">
-  <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Register</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9" crossorigin="anonymous">
-  </head>
-  <body>
+<?php
+
+require __DIR__ . '/../src/bootstrap.php';
+
+?>
+
+<?php view('header', ['title' => 'Register']); ?>
+
     <div class="container">
         <div class="row vh-100 justify-content-center align-items-center">
             <div class="col-md-6">
@@ -47,6 +46,4 @@
         </div>
     </div>
 
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-HwwvtgBNo3bZJJLYd8oVXjrBZt8cqVSpeBNS5n7C8IVInixGAoxmnlMuBnhbgrkm" crossorigin="anonymous"></script>
-  </body>
-</html>
+<?php view('footer'); ?>
